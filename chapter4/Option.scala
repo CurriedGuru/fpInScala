3688e0e1de859e7ffc3c443531f35f252b43e40d
Pushkar Kulkarni <pushkar.nk@in.ibm.com>
2019-03-31 09:39:49 +0530

    attempt: variance using flatMap

--- chapter4/Option.scala
+++ chapter4/Option.scala
@@ -33,6 +33,15 @@ trait Option[+A] {
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
+// Exercise 4.2: Implement the variance function in terms of flatMap
+def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.length)
+
+// For the variance function, the formula var(X) = mean(X^2)-(mean(X)^2) is used.
+// This formula is not typically used in real applications (catastrophic cancellation).
+def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs.map(x => math.pow(x, 2))).flatMap(x => Some(x - math.pow(mean(xs).getOrElse(0), 2)))
+}
+
 def half(x: Int): Option[Int] = if (x%2 == 1) None else Some(x/2)
 
 val x = Some(15)
@@ -44,3 +53,6 @@ println(x.map(x => x * 2).flatMap(x => half(x)))
 println(x.map(x => x * x).flatMap(x => half(x)).getOrElse(-1))
 println(x.map(x => x * x).flatMap(x => half(x)).orElse(Some(100)))
 println(x.map(x => x * x).flatMap(x => half(x)).orElse(Some(100)).filter(x => x % 12 == 0).getOrElse(0))
+
+val array = Array(3.0, 3.5, 4.0, 4.1)
+println(variance(array))
