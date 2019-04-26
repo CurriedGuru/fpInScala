8a75690be6d7d1f728cd06f00906e5aa782200b8
Pushkar Kulkarni <pushkar.nk@in.ibm.com>
2019-04-26 09:04:53 +0530

    Lifting APIs and making them Option oriented

--- chapter4/Option.scala
+++ chapter4/Option.scala
@@ -44,6 +44,18 @@ def variance(xs: Seq[Double]): Option[Double] = {
 
 def half(x: Int): Option[Int] = if (x%2 == 1) None else Some(x/2)
 
+// A function that lifts another function A => B to Option[A] => Option[B]
+def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+
+// Re-write the half function so that it returns an Int first
+def half0(x: Int): Int = x / 2
+
+// Lift half0
+val ohalf = lift(half0)
+println(ohalf(Some(12)))
+println(ohalf(Some(15)))
+
+// Tests
 val x = Some(15)
 println(x)
 println(x.map(x => x * x))
@@ -56,3 +68,16 @@ println(x.map(x => x * x).flatMap(x => half(x)).orElse(Some(100)).filter(x => x 
 
 val array = Array(3.0, 3.5, 4.0, 4.1)
 println(variance(array))
+
+// Convert a exception throwing function to an Option returning function, swallowing the exception
+val str = "Hello, World!"
+//println(str.charAt(13)) --- this throws an StringIndexOutOfBoundsException exception
+
+// Now let us wrap charAt into a function and return an Option instead
+def Try[A](a: => A): Option[A] =
+  try Some(a)
+  catch { case e: Exception => None }
+
+println(Try { str.charAt(4) })
+println(Try { str.charAt(13) })
+// Wow, lifting throwing APIs to Option-oriented APIs is so easy!
